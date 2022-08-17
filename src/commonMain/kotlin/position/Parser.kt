4b10ad9c88eca2f3ce6ebc42d3d92cf473548eb7
Daniel Famiyeh <daniel.kwaku.famiyeh@gmail.com>
2022-08-17 17:57:49 +0100

    Add docstrings to Parser class

--- src/commonMain/kotlin/position/Parser.kt
+++ src/commonMain/kotlin/position/Parser.kt
@@ -1,22 +1,40 @@
 package position
 
+/** Provides static methods to transform Position
+ * into standard chess notation and vice versa */
 class Parser {
     companion object {
         object PositionToNotation {
+            /**
+             * Transform board position into Forsyth-Edwards notation
+             * @param position State of the board
+             */
             fun fen(position: Position): String {
                 return TODO();
             }
 
+            /**
+             * Transform board position into portable game notation
+             * @param position State of the board
+             */
             fun pgn(position: Position): String {
                 return TODO();
             }
         }
 
         object NotationToPostion {
+            /**
+             * Transform FEN notation into a board Position
+             * @param notation FEN notation
+             */
             fun fen(notation: String): Position {
                 return TODO()
             }
 
+            /**
+             * Transform PGN notation into a board Position
+             * @param notation PGN notation
+             */
             fun pgn(notation: String): Position {
                 return TODO()
             }
