6a8af3a89add3a528914f1150424c4b7bd49e5ab
james <james-gibson@ntlworld.com>
2022-08-17 22:35:05 +0100

    Added logged and koin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ version = "1.0-SNAPSHOT"
 
 val koinVersion: String by project
 val logbackVersion: String by project
+val ktorVersion: String by project
 
 
 repositories {
@@ -54,9 +55,10 @@ kotlin {
                 implementation("io.insert-koin:koin-ktor:$koinVersion")
                 implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
                 // Ktor
-                implementation("io.ktor:ktor-server-netty:2.0.1")
-                implementation("io.ktor:ktor-server-html-builder-jvm:2.0.1")
+                implementation("io.ktor:ktor-server-netty:$ktorVersion")
+                implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+                implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
                 // Logger
                 implementation("ch.qos.logback:logback-classic:$logbackVersion")
             }
@@ -64,12 +66,19 @@ kotlin {
         val jvmTest by getting
         val jsMain by getting {
             dependencies {
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.0.0-pre.332-kotlin-1.6.21")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.0.0-pre.332-kotlin-1.6.21")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.0-pre.332-kotlin-1.6.21")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                // Koin Core features
+                implementation("io.insert-koin:koin-core:$koinVersion")
+// Koin Test features
             }
         }
-        val jsTest by getting
+        val jsTest by getting{
+            dependencies{ implementation("io.insert-koin:koin-test:$koinVersion") }
+        }
     }
 }
 
